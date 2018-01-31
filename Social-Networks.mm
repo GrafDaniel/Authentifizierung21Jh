5e22a34d70cff9d495cf9507c7fc73d25cbfa0cf
Daniel Graf <graf12@hm.edu>
2018-01-31 16:15:58 +0100

    Poster + Hilfsdateien

--- Social-Networks.mm
+++ Social-Networks.mm
@@ -1,77 +1,153 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1515344958040" ID="ID_506835981" MODIFIED="1515345425000" TEXT="Social Networks">
-<node CREATED="1515345208206" ID="ID_1374893052" MODIFIED="1515347402327" POSITION="left" STYLE="bubble" TEXT="Plattformen">
-<node CREATED="1515345227198" ID="ID_654693123" MODIFIED="1515345372626" TEXT="Xing"/>
-<node CREATED="1515345230782" ID="ID_1409956197" MODIFIED="1515345372626" TEXT="Facebook"/>
-<node CREATED="1515345249046" ID="ID_728226748" MODIFIED="1515345372626" TEXT="LinkedIn"/>
-<node CREATED="1515345286126" ID="ID_995256134" MODIFIED="1515345372626" TEXT="Instagram"/>
-<node CREATED="1515345291726" ID="ID_912519986" MODIFIED="1515345372626" TEXT="Snapchat"/>
+<node COLOR="#000000" CREATED="1515344958040" ID="ID_506835981" MODIFIED="1517405230786" TEXT="Social Networks">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1515345425862" HGAP="82" ID="ID_1968398412" MODIFIED="1517404967258" POSITION="right" STYLE="bubble" TEXT="Nutzung von Social Media Plattformen" VSHIFT="-1">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1515345460918" ID="ID_1431034152" MODIFIED="1517404660672" TEXT="5-30 Min/Tag">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
 </node>
-<node CREATED="1515345425862" ID="ID_1968398412" MODIFIED="1515347402327" POSITION="left" STYLE="bubble" TEXT="Nutzung">
-<node CREATED="1515345460918" ID="ID_1431034152" MODIFIED="1515345483825" TEXT="5-30 Min/Tag"/>
-<node CREATED="1515345473560" ID="ID_295275751" MODIFIED="1515345483825" TEXT="gar nicht"/>
-</node>
-<node CREATED="1515345042030" ID="ID_756857398" MODIFIED="1515345624573" POSITION="left" STYLE="bubble" TEXT="Sicherheit bestehender Netzwerke">
-<node CREATED="1515345571408" ID="ID_71394204" MODIFIED="1515345616374" TEXT="F&#xfc;r einige Benutzer ausreichend"/>
-<node CREATED="1515345626614" ID="ID_489399994" MODIFIED="1515345717842" TEXT="F&#xfc;r viele nicht ausreichend">
-<node CREATED="1515345655774" ID="ID_879532489" MODIFIED="1515345736360" TEXT="Besserung m&#xf6;glich durch">
-<node CREATED="1515345737630" ID="ID_1491710395" MODIFIED="1515345739472" TEXT="2-FA"/>
-<node CREATED="1515345741287" ID="ID_837026182" MODIFIED="1515345744488" TEXT="Always log out"/>
-<node CREATED="1515345746519" ID="ID_277404737" MODIFIED="1515345834537" TEXT="Erzwingen sicherer Passw&#xf6;rter"/>
+<node COLOR="#00b439" CREATED="1515345473560" ID="ID_295275751" MODIFIED="1517404660672" TEXT="gar nicht">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
 </node>
 </node>
-</node>
-<node CREATED="1515345906399" ID="ID_170603125" MODIFIED="1515346203272" POSITION="right" STYLE="bubble" TEXT="Konzept sinnvoll">
+<node COLOR="#0033ff" CREATED="1515345906399" HGAP="100" ID="ID_170603125" MODIFIED="1517404965087" POSITION="right" STYLE="bubble" TEXT="Sinnhafitgkeit des Konzeptes">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
 <icon BUILTIN="help"/>
-<node CREATED="1515345928487" ID="ID_1644557638" MODIFIED="1515346203272" TEXT="Viele finden es Sinnvoll"/>
-<node CREATED="1515345941375" ID="ID_812217160" MODIFIED="1515346203272" TEXT="Aber umst&#xe4;ndlich">
-<node CREATED="1515345957023" ID="ID_1371865968" MODIFIED="1515346203272" TEXT="K&#xf6;nnte Benutzer &#xfc;berfordern"/>
-<node CREATED="1515345971263" ID="ID_864818354" MODIFIED="1515346203272" TEXT="Dauert zu lange"/>
+<node COLOR="#00b439" CREATED="1515345928487" ID="ID_1644557638" MODIFIED="1517404660672" TEXT="Viele finden es Sinnvoll">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1515345941375" ID="ID_812217160" MODIFIED="1517404660672" TEXT="Aber umst&#xe4;ndlich">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1515345957023" ID="ID_1371865968" MODIFIED="1517404660672" TEXT="K&#xf6;nnte Benutzer &#xfc;berfordern">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1515345971263" ID="ID_864818354" MODIFIED="1517404916305" TEXT="Hoher Zeitaufwand">
+<font NAME="SansSerif" SIZE="14"/>
 </node>
 </node>
-<node CREATED="1515346190327" ID="ID_1278693219" MODIFIED="1515346203272" POSITION="right" STYLE="bubble" TEXT="Steigerung der Sicherheit">
+</node>
+<node COLOR="#0033ff" CREATED="1515346190327" HGAP="66" ID="ID_1278693219" MODIFIED="1517404861877" POSITION="right" STYLE="bubble" TEXT="Steigerung der Sicherheit" VSHIFT="10">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
 <icon BUILTIN="help"/>
-<node CREATED="1515346215223" ID="ID_452606276" MODIFIED="1515346258459" TEXT="Ja">
-<node CREATED="1515346341223" ID="ID_77795285" MODIFIED="1515346358674" TEXT="Aber sehr viele / individuelle Fake Accs notwendig"/>
+<node COLOR="#00b439" CREATED="1515346215223" ID="ID_452606276" MODIFIED="1517404660672" TEXT="Ja">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1515346341223" ID="ID_77795285" MODIFIED="1517404660672" TEXT="Aber sehr viele / individuelle Fake Accs notwendig">
+<font NAME="SansSerif" SIZE="14"/>
 </node>
-<node CREATED="1515346261495" ID="ID_704624651" MODIFIED="1515346263279" TEXT="Nein">
-<node CREATED="1515346266311" ID="ID_1510909856" MODIFIED="1515346333038" TEXT="Fake Profile lassen sich durchschauen / erkennen"/>
-<node CREATED="1515346414431" ID="ID_432817594" MODIFIED="1515346421295" TEXT="2-FA bereits ausreichend"/>
+</node>
+<node COLOR="#00b439" CREATED="1515346261495" ID="ID_704624651" MODIFIED="1517404660672" TEXT="Nein">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1515346266311" ID="ID_1510909856" MODIFIED="1517404660672" TEXT="Fake Profile lassen sich durchschauen / erkennen">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1515346414431" ID="ID_432817594" MODIFIED="1517405003970" TEXT="2-Factor Authetification bereits ausreichend">
+<font NAME="SansSerif" SIZE="14"/>
 </node>
 </node>
-<node CREATED="1515346525391" ID="ID_117427915" MODIFIED="1515346609670" POSITION="right" STYLE="bubble" TEXT="W&#xfc;rden Sie das Netzwerk nutzen">
+</node>
+<node COLOR="#0033ff" CREATED="1515346525391" ID="ID_117427915" MODIFIED="1517404953884" POSITION="right" STYLE="bubble" TEXT="Bereitschaft das Netzwerk zu nutzen">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
 <icon BUILTIN="help"/>
-<node CREATED="1515346543927" ID="ID_1081693938" MODIFIED="1515346562440" TEXT="Nicht wegen des Login konzepts"/>
-<node CREATED="1515346566479" ID="ID_658453967" MODIFIED="1515346602924" TEXT="Features / Nutzung unter Freunden ausschlaggebend"/>
-<node CREATED="1515346688880" ID="ID_1608258613" MODIFIED="1515346697688" TEXT="Kein weiteres Netzwerk notwendig"/>
-<node CREATED="1515346632895" ID="ID_693575981" MODIFIED="1515346653724" TEXT="Sinnvoller: Konzept auf bestehendem SN einf&#xfc;hren"/>
+<node COLOR="#00b439" CREATED="1515346543927" ID="ID_1081693938" MODIFIED="1517404660672" TEXT="Nicht wegen des Login konzepts">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
 </node>
-<node CREATED="1515346049880" ID="ID_705189412" MODIFIED="1515346800521" POSITION="left" STYLE="bubble" TEXT="Weitere Fragen / Ideen / Aussagen">
-<node CREATED="1515346058567" ID="ID_1370455385" MODIFIED="1515346203272" TEXT="Was passiert wenn PW vergessen?"/>
-<node CREATED="1515346488623" ID="ID_177410559" MODIFIED="1515346489841" TEXT="2-FA auf Handy sinnfrei, da auf gleichem Ger&#xe4;t"/>
-<node CREATED="1515346803535" ID="ID_347849542" MODIFIED="1515346825942" TEXT="Es ist sicher Interessant, die Fake Profile anderer durchzusehen"/>
-<node CREATED="1515347108400" ID="ID_1568604636" MODIFIED="1515347116465" TEXT="Hilft nur, wenn Angreifer mich nicht kennt"/>
-<node CREATED="1515347167944" ID="ID_331939347" MODIFIED="1515347181487" TEXT="Ist mein Fake Profil dann Googlebar?"/>
-<node CREATED="1515347232912" ID="ID_1609156986" MODIFIED="1515347241954" TEXT="Was passiert wenn das Fake Profil jemanden anschreibt?"/>
-<node CREATED="1515347277120" ID="ID_465825346" MODIFIED="1515347291616" TEXT="Fake Profile posten ja nichts"/>
-<node CREATED="1515347656337" ID="ID_777621053" MODIFIED="1515347665474" TEXT="Ich glaube, ich werde nicht gehackt"/>
+<node COLOR="#00b439" CREATED="1515346566479" ID="ID_658453967" MODIFIED="1517404660672" TEXT="Features / Nutzung unter Freunden ausschlaggebend">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
 </node>
-<node CREATED="1515346755592" ID="ID_417739308" MODIFIED="1515346772037" POSITION="right" STYLE="bubble" TEXT="Bereitschaft, Fake Profil selbst zu erstellen">
-<node CREATED="1515346857592" ID="ID_130213538" MODIFIED="1515346863691" TEXT="Viele haben dazu keine Lust"/>
-<node CREATED="1515346865912" ID="ID_1710068162" MODIFIED="1515346901721" TEXT="Ja">
-<node CREATED="1515346903184" ID="ID_1762980750" MODIFIED="1515346920799" TEXT="Kann sogar Spa&#xdf; bereiten"/>
-<node CREATED="1515346923712" ID="ID_1340921259" MODIFIED="1515346927399" TEXT="Darf nicht lange dauern"/>
-<node CREATED="1515346936080" ID="ID_444180646" MODIFIED="1515346943428" TEXT="Man darf nicht gezwungen sein"/>
-<node CREATED="1515346948680" ID="ID_537564175" MODIFIED="1515346956435" TEXT="Muss auch &quot;sp&#xe4;ter&quot; m&#xf6;glich sein"/>
+<node COLOR="#00b439" CREATED="1515346688880" ID="ID_1608258613" MODIFIED="1517404660672" TEXT="Kein weiteres Netzwerk notwendig">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1515346632895" ID="ID_693575981" MODIFIED="1517405045780" TEXT="Sinnvoller: Konzept in bestehendes Social Network integrieren">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
 </node>
 </node>
-<node CREATED="1515347019656" ID="ID_205743177" MODIFIED="1515347487011" POSITION="right" STYLE="bubble" TEXT="Bereitschaft, Daten f&#xfc;r Profilgenerierung bereitzustellen">
-<node CREATED="1515347076128" ID="ID_139425693" MODIFIED="1515347083715" TEXT="Problem: Bilderrechte"/>
-<node CREATED="1515347411368" ID="ID_299125439" MODIFIED="1515347419326" TEXT="Die meisten Nutzer w&#xe4;ren dazu bereit"/>
-<node CREATED="1515347439744" ID="ID_1727658549" MODIFIED="1515347564662" TEXT="Umsetzung 2+4 hat die meisten Anh&#xe4;nger">
-<node CREATED="1515347565944" ID="ID_1551446979" MODIFIED="1515347569082" TEXT="Weil einfach"/>
-<node CREATED="1515347571041" ID="ID_1000098295" MODIFIED="1515347576163" TEXT="und authentische Profile"/>
+<node COLOR="#0033ff" CREATED="1515346755592" ID="ID_417739308" MODIFIED="1517404660688" POSITION="right" STYLE="bubble" TEXT="Bereitschaft, Fake Profil selbst zu erstellen">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1515346857592" ID="ID_130213538" MODIFIED="1517405108644" TEXT="Nein">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1517405065069" ID="ID_589715374" MODIFIED="1517405073701" TEXT="Zu Zeitaufw&#xe4;ndig">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1517405078456" ID="ID_841564346" MODIFIED="1517405091024" TEXT="Keine Bereitschaft Daten zur Verf&#xfc;gung zu stellen">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1515346865912" ID="ID_1710068162" MODIFIED="1517404660688" TEXT="Ja">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1515346903184" ID="ID_1762980750" MODIFIED="1517404660688" TEXT="Kann sogar Spa&#xdf; bereiten">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1515346923712" ID="ID_1340921259" MODIFIED="1517404660688" TEXT="Darf nicht lange dauern">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1515346936080" ID="ID_444180646" MODIFIED="1517404660688" TEXT="Man darf nicht gezwungen sein">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1515346948680" ID="ID_537564175" MODIFIED="1517404660688" TEXT="Muss auch &quot;sp&#xe4;ter&quot; m&#xf6;glich sein">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1515345042030" HGAP="97" ID="ID_756857398" MODIFIED="1517404972881" POSITION="right" STYLE="bubble" TEXT="Sicherheit bestehender Netzwerke" VSHIFT="5">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1515345571408" ID="ID_71394204" MODIFIED="1517404660672" TEXT="F&#xfc;r einige Benutzer ausreichend">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1515345626614" ID="ID_489399994" MODIFIED="1517404660672" TEXT="F&#xfc;r viele nicht ausreichend">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1515345655774" ID="ID_879532489" MODIFIED="1517404660672" TEXT="Besserung m&#xf6;glich durch">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1515345737630" ID="ID_1491710395" MODIFIED="1517405137686" TEXT="2-Factor Authentification"/>
+<node COLOR="#111111" CREATED="1515345741287" ID="ID_837026182" MODIFIED="1517404660672" TEXT="Always log out"/>
+<node COLOR="#111111" CREATED="1515345746519" ID="ID_277404737" MODIFIED="1517404660672" TEXT="Erzwingen sicherer Passw&#xf6;rter"/>
+</node>
+<node COLOR="#990000" CREATED="1517405167413" ID="ID_1555307878" MODIFIED="1517405174252" TEXT="Ausspionieren von Daten">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1515347019656" HGAP="85" ID="ID_205743177" MODIFIED="1517404975769" POSITION="right" STYLE="bubble" TEXT="Bereitschaft, Daten f&#xfc;r Profilgenerierung bereitzustellen" VSHIFT="-9">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1515347076128" ID="ID_139425693" MODIFIED="1517404660688" TEXT="Problem: Bilderrechte">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1515347411368" ID="ID_299125439" MODIFIED="1517404660688" TEXT="Die meisten Nutzer w&#xe4;ren dazu bereit">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1515347439744" ID="ID_1727658549" MODIFIED="1517404660688" TEXT="Umsetzung 2+4 hat die meisten Anh&#xe4;nger">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1515347565944" ID="ID_1551446979" MODIFIED="1517405208132" TEXT="einfach f&#xfc;r den Benutzer">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1515347571041" ID="ID_1000098295" MODIFIED="1517405219818" TEXT="authentische Fakeprofile">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
 </node>
 </node>
 </node>
